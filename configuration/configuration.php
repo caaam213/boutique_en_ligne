b5c9cc16ac1dac53ebc44d8e33a8ad0ff9a4e37a
Camelia <camelia.meraoui123@gmail.com>
2021-12-02 17:24:27 +0100

    Configuration file update

---
+++ configuration/configuration.php
@@ -0,0 +1,26 @@
+<?php
+    //Database access
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'boutique_en_ligne';
+    const DB_USER = 'root';
+    const DB_MDP = '';
+
+    //Authors name
+    define('AUTHORS',array('Houria REHAB','Laura TOBAJAS', 'Camélia MERAOUI'));
+
+    //Root folders
+    define('CLASSES_PATH','./classes/');
+    define('CONTROLLERS_PATH','./controllers/c_');
+    define('MODELS_PATH','./models/');
+    define('RESOURCES_PATH','./resources/');
+    define('VIEWS_PATH','./views/v_');
+
+    //Subfolders : 
+
+    //Styles
+    define('STYLES_PATH',RESOURCES_PATH.'styles/');
+
+    //Images
+    define('PRODUCT_IMAGES',RESOURCES_PATH.'productimages/');
+
+?>
